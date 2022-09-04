dbe8499a276ac08bb67b3aad7ec894c64d630f8f
ReiLoko4 <103978193+ReiLoko4@users.noreply.github.com>
2022-09-04 14:53:04 -0300

    Update TMVD.py

--- TMVD.py
+++ TMVD.py
@@ -1,34 +1,57 @@
 import PySimpleGUI as sg
 from yt_dlp import YoutubeDL
 from pathlib import Path
+import os
 
 
 def download_music(url):
     try:
-        window["out"].update("Baixando")
-        out = str(Path.home()/"Documents"/"Trash downloader"/"Musics (trash)"/"%(title)s.%(ext)s")
-        with YoutubeDL({"ffmpeg_location":"ffmpeg.exe", "outtmpl": out, "format":"140"}) as ydl:
+        print("Baixando")
+        out = str(
+            Path.home()
+            / "Documents"
+            / "Trash downloader"
+            / "Musics (trash)"
+            / "%(title)s.%(ext)s"
+        )
+        with YoutubeDL(
+            {"ffmpeg_location": "ffmpeg.exe", "outtmpl": out, "format": "140"}
+        ) as ydl:
             ydl.download(url)
     except:
-        window["out"].update("download mal sucedido, verifique a Url.")
+        print("download mal sucedido, verifique a Url.")
+
 
 def download_video(url):
     try:
-        window["out"].update("Baixando")
-        out = str(Path.home()/"Documents"/"Trash downloader"/"Videos (trash)"/"%(title)s.%(ext)s")
-        with YoutubeDL({"ffmpeg_location":"ffmpeg.exe", "outtmpl": out, "format":"bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b"}) as ydl:
+        print("Baixando")
+        out = str(
+            Path.home()
+            / "Documents"
+            / "Trash downloader"
+            / "Videos (trash)"
+            / "%(title)s.%(ext)s"
+        )
+        with YoutubeDL(
+            {
+                "ffmpeg_location": "ffmpeg.exe",
+                "outtmpl": out,
+                "format": "bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4] / bv*+ba/b",
+            }
+        ) as ydl:
             ydl.download(url)
     except:
-        window["out"].update("download mal sucedido, verifique a Url.")
+        print("download mal sucedido, verifique a Url.")
+
 
 def get_quality(url):
     pass
 
-layout = [[sg.Output(key = "out",size = (50,10))],
 
-[sg.ButtonMenu("Video", [["Video", "Music"], ["Video", "Music"]], key = "slctf") ],
-
-[sg.Input(key = "IN"), sg.Button("download"), sg.Button("Exit", key="Close")]
+layout = [
+    [sg.Output(key="out", size=(50, 10))],
+    [sg.ButtonMenu("Video", [["Video", "Music"], ["Video", "Music"]], key="slctf")],
+    [sg.Input(key="IN"), sg.Button("download"), sg.Button("Exit", key="Close")],
 ]
 
 
@@ -37,34 +60,28 @@ window = sg.Window("nada aqui", layout)
 while True:
     event, values = window.read()
 
-
     if event == "slctf":
         if values["slctf"] == "Music":
             values["slctf"] = "Music"
         else:
-            values["slctf"] ="Video"
-        
+            values["slctf"] = "Video"
 
     if event == "download":
         if values["slctf"] == "Video":
-            if len(values["IN"]) > 0:              
+            if len(values["IN"]) > 0:
                 download_video(values["IN"])
             else:
-                window["out"].add("Por favor digite algo.")
+                print("Por favor digite algo.")
         else:
-            if len(values["IN"]) > 0:              
+            if len(values["IN"]) > 0:
                 download_music(values["IN"])
             else:
-                window["out"].add("Por favor digite algo.")
-            
-
+                print("Por favor digite algo.")
 
     if event in (None, "Close"):
         break
-    
-    
 
 
 window.close()
 
-#new code looks sexy!
+# new code looks sexy!
